39a7aff842061408eb949166c6cce683a951dc41
Logan Hanaway <lthanaway14@gmail.com>
2024-05-09 15:27:15 -0500

    18 done

--- 18_calculate/calculate.py
+++ 18_calculate/calculate.py
@@ -26,3 +26,22 @@ def calculate(operation, a, b, make_int=False, message='The result is'):
         >>> calculate('foo', 2, 3)
         
     """
+    result = None
+    if operation == 'add':
+        result = a + b
+    elif operation == 'subtract':
+        result = a - b
+    elif operation == 'multiply':
+        result = a * b
+    elif operation == 'divide':
+        if b == 0:
+            return None
+        result = a / b
+    else:
+        return None
+    
+    if make_int:
+        result = int(result)
+        
+    return f"{message} {result}"
+
