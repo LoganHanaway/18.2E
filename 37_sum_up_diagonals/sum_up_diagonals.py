e0c6f61e30d0fbe7e6e4f8f689ff4edd8af65e32
Logan Hanaway <lthanaway14@gmail.com>
2024-06-06 14:34:10 -0500

    through 31 done

--- 37_sum_up_diagonals/sum_up_diagonals.py
+++ 37_sum_up_diagonals/sum_up_diagonals.py
@@ -18,3 +18,7 @@ def sum_up_diagonals(matrix):
         >>> sum_up_diagonals(m2)
         30
     """
+    n = len(matrix)
+    tl_br_sum = sum(matrix[i][i] for i in range(n))
+    bl_tr_sum = sum(matrix[i][n-1-i] for i in range(n))
+    return tl_br_sum + bl_tr_sum
